9e1e0908e0fdd3b3ad614ace4809f77ec1f3a4be
kent <nwekep14@gmail.com>
2024-09-20 02:20:29 +0100

    add vercel to app

---
+++ backend/api/index.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const bodyParser = require("body-parser");
+const path = require("path");
+const userRoutes = require("../routes/user");
+const invoicesRoutes = require("../routes/invoices");
+const authRoutes = require("../routes/auth");
+
+const app = express();
+app.set("view engine", "pug");
+app.set("views", "views");
+app.use(cors());
+
+const port = 8080;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/users", userRoutes);
+app.use(invoicesRoutes);
+app.use(authRoutes);
+
+app.get("/", (req, res) => {
+	res.send("Hello, World!");
+});
+
+app.listen(port, () => {
+	console.log(`Server is running on http://localhost:${port}`);
+});
