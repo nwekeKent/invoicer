8464b3df226113c6823e270c96f03a691ba2e168
kent <nwekep14@gmail.com>
2024-09-22 01:26:19 +0100

    edit cors message

--- backend/api/index.js
+++ backend/api/index.js
@@ -16,6 +16,27 @@ app.use(cors());
 
 const port = 8080;
 
+const allowedOrigins = [
+	"https://invoicer-app-azure.vercel.app",
+	"http://localhost:3000",
+	"http://localhost:8000",
+];
+
+app.use(
+	cors({
+		origin: function (origin, callback) {
+			// Allow requests with no origin (like mobile apps, curl, etc.)
+			if (!origin) return callback(null, true);
+			if (allowedOrigins.indexOf(origin) === -1) {
+				const msg =
+					"The CORS policy for this site does not allow access from the specified Origin.";
+				return callback(new Error(msg), false);
+			}
+			return callback(null, true);
+		},
+	})
+);
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
