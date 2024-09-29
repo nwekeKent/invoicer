c63764c57d08c713cd0f3c123a7c41486ed818ed
nwekekent <nwekep14@gmail.com>
2024-09-29 02:45:45 +0100

    remove service account

--- backend/database/database.js
+++ backend/database/database.js
@@ -1,25 +1,5 @@
-const admin = require("firebase-admin");
 const firebase = require("firebase/app");
 const { getAuth } = require("firebase/auth");
-
-const serviceAccount = {
-	type: process.env.FIREBASE_TYPE,
-	project_id: process.env.FIREBASE_PROJECTID,
-	private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-	private_key: process.env.FIREBASE_PRIVATE_KEY,
-	client_email: process.env.FIREBASE_CLIENT_EMAIL,
-	client_id: process.env.FIREBASE_CLIENT_ID,
-	auth_uri: process.env.FIREBASE_AUTH_URI,
-	token_uri: process.env.FIREBASE_TOKEN_URI,
-	auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
-	client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
-	universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
-};
-
-admin.initializeApp({
-	credential: admin.credential.cert(serviceAccount),
-	databaseURL: process.env.FIREBASE_DATABASE_URL,
-});
 
 const firebaseConfig = {
 	apiKey: process.env.FIREBASE_API_KEY,
@@ -33,7 +13,7 @@ const firebaseConfig = {
 
 const myApp = firebase.initializeApp(firebaseConfig);
 
-const db = admin.firestore();
-const auth = admin.auth();
+const db = myApp.firestore();
+const auth = myApp.auth();
 const clientAuth = getAuth(myApp);
 module.exports = { db, auth, clientAuth };
