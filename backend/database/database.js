6bd4675daeda154d96a6ab0afaa0a3298b0592bd
nwekekent <nwekep14@gmail.com>
2024-09-29 02:16:31 +0100

    - fix env

--- backend/database/database.js
+++ backend/database/database.js
@@ -2,7 +2,19 @@ const admin = require("firebase-admin");
 const firebase = require("firebase/app");
 const { getAuth } = require("firebase/auth");
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+const serviceAccount = {
+	type: process.env.FIREBASE_TYPE,
+	project_id: process.env.FIREBASE_PROJECT_ID,
+	private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+	private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"), // Unescape newlines
+	client_email: process.env.FIREBASE_CLIENT_EMAIL,
+	client_id: process.env.FIREBASE_CLIENT_ID,
+	auth_uri: process.env.FIREBASE_AUTH_URI,
+	token_uri: process.env.FIREBASE_TOKEN_URI,
+	auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+	client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+	universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+};
 
 admin.initializeApp({
 	credential: admin.credential.cert(serviceAccount),
