fb168ab9ff68a438c500b7f6d5fb4f7528a819de
nwekekent <nwekep14@gmail.com>
2025-08-25 22:15:28 +0100

    - update auth hooks

--- frontend/src/api/mutations/auth/hooks.ts
+++ frontend/src/api/mutations/auth/hooks.ts
@@ -6,15 +6,51 @@ import {
 	register,
 	AuthResponse,
 } from "@/api";
+import { Toast } from "@/components/shared/Toast";
+import { formatErrorMsg } from "@/utils";
+import { useRouter } from "next/navigation";
 
 export const useLogin = () => {
+	const router = useRouter();
 	return useMutation<AuthResponse, Error, LoginPayload>({
 		mutationFn: payload => login(payload),
+		onSuccess: response => {
+			localStorage.setItem("token", response.data.token);
+			localStorage.setItem("user", JSON.stringify(response.data.user));
+			Toast.fire({
+				icon: "success",
+				title: response.message || "Login successful!",
+			});
+			router.push("/invoices");
+		},
+		onError: (error: Error) => {
+			Toast.fire({
+				icon: "error",
+				title: formatErrorMsg(error.message) || "Failed to login!",
+			});
+		},
 	});
 };
 
 export const useRegister = () => {
+	const router = useRouter();
 	return useMutation<AuthResponse, Error, RegisterPayload>({
 		mutationFn: payload => register(payload),
+		onSuccess: response => {
+			localStorage.setItem("token", response.data.token);
+			localStorage.setItem("user", JSON.stringify(response.data.user));
+			Toast.fire({
+				icon: "success",
+				title: response.message || "User registered successful!",
+			});
+
+			router.push("/invoices");
+		},
+		onError: (error: Error) => {
+			Toast.fire({
+				icon: "error",
+				title: formatErrorMsg(error.message) || "Failed to register user!",
+			});
+		},
 	});
 };
